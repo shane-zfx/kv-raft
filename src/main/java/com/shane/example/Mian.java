4d193dfaae7eb390616e0e931af8c10183c334d6
shane <Zzfx_7@163.com>
2023-08-31 17:40:04 +0800

    feat<node>:init

--- src/main/java/com/shane/example/Mian.java
+++ src/main/java/com/shane/example/Mian.java
@@ -3,6 +3,7 @@ package com.shane.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.SchemaOutputResolver;
 import javax.xml.transform.Source;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,27 +15,32 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Mian {
 
-    private static final ExecutorService executorService = new ThreadPoolExecutor(5,
-            10,
-            5,
-            TimeUnit.SECONDS,
-            new SynchronousQueue<>());
+    private static final ExecutorService executorService = new ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, new SynchronousQueue<>());
 
     private final AtomicInteger ctl = new AtomicInteger(ctlOf(RUNNING, 0));
     private static final int COUNT_BITS = Integer.SIZE - 3;
-    private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
+    private static final int CAPACITY = (1 << COUNT_BITS) - 1;
 
     // runState is stored in the high-order bits
-    private static final int RUNNING    = -1 << COUNT_BITS;
-    private static final int SHUTDOWN   =  0 << COUNT_BITS;
-    private static final int STOP       =  1 << COUNT_BITS;
-    private static final int TIDYING    =  2 << COUNT_BITS;
-    private static final int TERMINATED =  3 << COUNT_BITS;
+    private static final int RUNNING = -1 << COUNT_BITS;
+    private static final int SHUTDOWN = 0 << COUNT_BITS;
+    private static final int STOP = 1 << COUNT_BITS;
+    private static final int TIDYING = 2 << COUNT_BITS;
+    private static final int TERMINATED = 3 << COUNT_BITS;
 
     // Packing and unpacking ctl
-    private static int runStateOf(int c)     { return c & ~CAPACITY; }
-    private static int workerCountOf(int c)  { return c & CAPACITY; }
-    private static int ctlOf(int rs, int wc) { return rs | wc; }
+    private static int runStateOf(int c) {
+        return c & ~CAPACITY;
+    }
+
+    private static int workerCountOf(int c) {
+        return c & CAPACITY;
+    }
+
+    private static int ctlOf(int rs, int wc) {
+        return rs | wc;
+    }
+
     public static void main(String[] args) {
 //        System.out.println(RUNNING);
 //        System.out.println(SHUTDOWN);
@@ -44,8 +50,8 @@ public class Mian {
 //        System.out.println(~CAPACITY);
 
 
-        System.out.println(Integer.toBinaryString(ctlOf(RUNNING, 0)));
-        //
+//        System.out.println(Integer.toBinaryString(ctlOf(RUNNING, 0)));
+        executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
 
     }
 }
